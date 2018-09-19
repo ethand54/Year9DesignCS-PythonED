2fc6a564c553e190cf7a69b2be95ccbbaeaecee7
Ethan Dawood <ethan.dawood@Y22-Design-Ethan-Dawood.local>
2018-09-19 12:16:03 -0400

    update calc

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -15,14 +15,12 @@ y2 = input("Input y2: ")
 y2 = int(y2)
 
 
-
-
 #Process
 rise = x2 - x1
 run = y2 - y1
 
-
+fSlope = rise/run
 
 #Output
-print(rise)
-print(run)
+print("Your Slope is m="+str(rise)+"/"+str(run))
+print("Yours slope as a decimal is " +str(fSlope))
