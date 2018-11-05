e9823c8a1592a91ce3b53ca8c0884460aec356cc
Ethan Dawood <ethan.dawood@Ethan-Dawood.local>
2018-11-05 12:19:08 -0500

    Gui practice work period

---
+++ ClassOrganizer.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Class Organizer")
+entry1 = tk.Entry(root)
+entry1.pack()
+root.mainloop()
+window.geometry("600x780")
--- Contesst questions/CCC2018J1ED.py
+++ Contesst questions/CCC2018J1ED.py
@@ -1,1 +1,5 @@
 print("CCC2018J1")
+#when executing the file that is in 2 folders do ls to see what the folder
+#is named then cd which is change directory and type in the name of the 
+#folder that you are tying to get to. then ls again to see what the file
+#is named then python3 then the file name
