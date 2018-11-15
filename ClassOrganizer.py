2ff79d211dffdd0918a8bf465918892524d15fff
Ethan Dawood <ethan.dawood@Ethan-Dawood.local>
2018-11-15 14:12:27 -0500

    update

--- ClassOrganizer.py
+++ ClassOrganizer.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.title("Class Organizer")
 entry1 = tk.Entry(root)
 entry1.pack()
-canvas1 = tk.Canvas(root, width = 1920, height = 1080)
+canvas1 = tk.Canvas(root, width =1024, height =576, background = "grey")
 
 canvas1.pack();
 root.mainloop()
