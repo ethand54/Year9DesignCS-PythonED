7d107d623a11a309ec6d1d2fe5f4651f0aad9fd1
Ethan Dawood <ethan.dawood@Ethan-Dawood.local>
2018-11-19 11:08:56 -0500

    Practice GUI

--- ClassOrganizer.py
+++ ClassOrganizer.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.title("Class Organizer")
 entry1 = tk.Entry(root)
 entry1.pack()
-canvas1 = tk.Canvas(root, width =1024, height =576, background = "grey")
+canvas1 = tk.Canvas(root, width =1024, height =576)
 
 canvas1.pack();
 root.mainloop()
---
+++ Project/GUI1ED.py
@@ -0,0 +1,26 @@
+#This imports the tkinter "tool box" this 
+#contains all the support material
+#to make GUI elements. by including "as tk" 
+#we are giving a short name to use
+import tkinter as tk 
+
+
+
+root = tk.Tk()
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+#We are writing an EVENT DRIVE PROGRAM.
+#Build the GUI
+#Start it running
+#Wait for 'Event'
+#Starts the program
