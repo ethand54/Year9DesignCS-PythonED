22440ed021140185fff30e52977241c9dc059927
Ethan Dawood <ethan.dawood@Ethan-Dawood.local>
2018-12-17 08:18:42 -0500

    Project

---
+++ Project/GUIDropDownMenu.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+
+def change(*args):
+	print("running change")
+	print(var.get())
+
+root = tk.Tk() #Constructs our main window
+
+#List of strings 
+#My list is called options there are 3 elements with index 0 to 2
+#print(OPTIONS[2])
+OPTIONS = [
+	"Understand",
+	"OK",
+	"Help"
+]
+
+var = tk.StringVar(root)
+var.set(OPTIONS[0])
+var.trace("w",change)
+
+dropDownMenu = tk.OptionMenu(root,var, OPTIONS[0],OPTIONS[1],OPTIONS[2])
+dropDownMenu.pack()
+
+word1Label = tk.Label(root, text = "Class-Topic", foreground = "blue")
+word1Label.pack()
+
+
+ent1 = tk.Entry(root)
+ent1.pack()
+
+
+btnGO = tk.Button(root, text = "ENTER")
+btnGO.pack()
+
+print(ent1)
+
+root = tk.Tk()
+
+
+
+
+
+root.mainloop()
+print("End program")
--- Project/projectFrontEnd.py
+++ Project/projectFrontEnd.py
@@ -15,43 +15,16 @@ output = tk.Text(root, height = 5, width = 50)
 output.config(state = "disable")
 output.grid(row = 1, column = 0, columnspan = 4)
 
-word1Label = tk.Label(root, text = "Class-Topic-Understading", foreground = "blue")
-word1Label.grid(row = 1, column = 0)
+word1Label = tk.Label(root, text = "Class-Topic", foreground = "blue")
+word1Label.pack
 
-ent1 = tk.Entry(root)
-ent1.grid(row = 1, column = 1)
+#ent1 = tk.Entry(root)
+#ent1.grid(row = 1, column = 1)
 
 
 btnGO = tk.Button(root, text = "ENTER")
 btnGO.grid(row = 30,column = 0)\
 
-master = tk.Tk()
-
-w = tk.Canvas(master, width=200, height=100)
-w.pack()
-
-
-w.create_line(0, 0, 200, 100)
-w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-
-w.create_rectangle(50, 25, 150, 75, fill="blue")
-
-#	print("running change")
-#	print(var.get())
-
-
-#OPTIONS = [
-#	"Sort",
-#	"Alphebetical",
-#	"Understanding",
-#]
-
-#var.set(OPTIONS[0])
-#var.trace("w",change)
-
-#dropDownMenu = tk.OptionMenu(root,var, OPTIONS[0],OPTIONS[1],OPTIONS[2])
-#dropDownMenu.pack()
-#dropDownMenu.grid(row = 3, column = 1)
 
 
 root.mainloop()
