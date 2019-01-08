8fcf771a9b0c1ccabe852c12539e4f085078c647
Ethan Dawood <ethan.dawood@Ethan-Dawood.local>
2019-01-08 08:48:32 -0500

    FINAL PROJECT UPDATE

--- Project/CodingProject.py
+++ Project/CodingProject.py
@@ -1,45 +1,61 @@
 import tkinter as tk
 
+
+
+
 def change(*args):
 	print("running change")
 	print(var.get())
 
+def AddToList():
+	#label2 = tk.Label(root, text = ent1.get() + "-" + var.get(), foreground="blue")
+	#label2.pack()
+	f = open("file.txt", "a+")
+	f.write(ent1.get() + " - " + ent2.get() + " : " + var.get() + "\n")
+	f.close()
+	print("file written")
+
+def ClearFile(): 
+	f = open("file.txt", "w+")
+	f.write("")
+	f.close()
+
 root = tk.Tk() #Constructs our main window
 
-#List of strings 
-#My list is called options there are 3 elements with index 0 to 2
-#print(OPTIONS[2])
+
 OPTIONS = [
-	"Understand",
-	"OK",
-	"Help"
+	"Strong Understanding",
+	"Basic Understanding",
+	"Weak Understanding"
 ]
 
 var = tk.StringVar(root)
-var.set(OPTIONS[0])
+var.set(OPTIONS[2])
 var.trace("w",change)
 
 dropDownMenu = tk.OptionMenu(root,var, OPTIONS[0],OPTIONS[1],OPTIONS[2])
 dropDownMenu.pack()
 
-word1Label = tk.Label(root, text = "Class-Topic", foreground = "blue")
+word1Label = tk.Label(root, text = "Class", foreground = "red")
 word1Label.pack()
-
 
 ent1 = tk.Entry(root)
 ent1.pack()
 
+word2Label = tk.Label(root, text = "Topic", foreground = "black")
+word2Label.pack()
 
-btnGO = tk.Button(root, text = "ENTER")
+ent2 = tk.Entry(root)
+ent2.pack()
+
+
+
+
+btnGO = tk.Button(root, text = "ENTER", command=AddToList)
 btnGO.pack()
 
-
-
-root = tk.Tk()
-
-
-
+clearButton = tk.Button(root, text = "CLEAR FILE", command=ClearFile)
+clearButton.pack()
 
 
 root.mainloop()
-print("End program")
